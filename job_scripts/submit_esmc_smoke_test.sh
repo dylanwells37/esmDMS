bbcbbb83f519286c5ca42e9e88ec8a0f9554fadb
Dylan Wells <dhw28@n015.csb.pitt.edu>
2026-06-03 10:54:45 -0400

    BLAH

--- job_scripts/submit_esmc_smoke_test.sh
+++ job_scripts/submit_esmc_smoke_test.sh
@@ -31,7 +31,7 @@ export HF_HOME="${HF_HOME:-$HOME/.cache/huggingface}"
 export ESMDMS_TORCH_DTYPE="${ESMDMS_TORCH_DTYPE:-bfloat16}"
 
 # Set ESMC_MODEL=biohub/ESMC-6B to actually exercise the 6B path.
-export ESMC_MODEL="${ESMC_MODEL:-biohub/ESMC-300M}"
+export ESMC_MODEL="${ESMC_MODEL:-biohub/ESMC-6B}"
 
 # HF_TOKEN must be exported in your shell environment for gated models.
 if [ -z "${HF_TOKEN:-}" ]; then
